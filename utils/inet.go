4cf1ebfbae71427e97834f042e38ccc98eba7dde
Maciej Mionskowski <business@maciekmm.net>
2015-08-09 01:24:50 +0200

    Sleep between request retries

--- utils/inet.go
+++ utils/inet.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"errors"
 	"net/http"
+	"time"
 )
 
 func RetryRequest(tries int, client *http.Client, request *http.Request) (*http.Response, error) {
@@ -10,7 +11,13 @@ func RetryRequest(tries int, client *http.Client, request *http.Request) (*http.
 	var err error
 	for try := 0; try < tries; try++ {
 		resp, err = client.Do(request)
+		if err != nil {
+			time.Sleep(1 * time.Second)
+			continue
+		}
 		if resp.StatusCode != http.StatusOK {
+			resp.Body.Close()
+			time.Sleep(2 * time.Second)
 			continue
 		}
 		return resp, err
